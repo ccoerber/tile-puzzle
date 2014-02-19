49175b7accb5ed3e07c780e43763bfd016abac78
ccoerber <chris.coerber@outlook.com>
2014-02-19 14:39:13 -0800

    Converting to a more recursive type solution. Fixed token matching bug.

--- main.cpp
+++ main.cpp
@@ -13,6 +13,8 @@ std::stack<Tile*> solvedStack;
 // tile2 all the way around until match is found
 bool matchTiles(Tile* tile1, Tile* tile2, int pos1, int pos2);
 
+bool matchTokens(Token tok1, Token tok2);
+
 // Dumps an ascii representation of the current state of the solved puzzle
 void dumpTiles();
 
@@ -22,9 +24,11 @@ const char* tokToStr(Token);
 // Run some tests to ensure algorithm is working
 void runTests();
 
+void findNextMatch();
+
 int main()
 {
-    allTiles.push(new Tile(1, TOK_PG_B, TOK_BAR_A, TOK_GG_A, TOK_TREE_A));
+    allTiles.push(new Tile(1, TOK_PG_B, TOK_BAR_A, TOK_GG_B, TOK_TREE_A));
     allTiles.push(new Tile(2, TOK_TREE_B, TOK_BAR_B, TOK_GG_B, TOK_PG_B));
     allTiles.push(new Tile(3, TOK_GG_B, TOK_PG_A, TOK_GG_A, TOK_BAR_A));
     allTiles.push(new Tile(4, TOK_BAR_A, TOK_GG_B, TOK_PG_A, TOK_TREE_A));
@@ -36,36 +40,48 @@ int main()
     int iterations = 0;
     int lastIdMatched = -1;
 
-    while (allTiles.size() > 0 && iterations < max_iterations) {
+    int cur = 0;
+    while (cur < allTiles.size()) {
+    	findNextMatch();
+    	cur++;
+    }
 
-    	Tile* tile = solvedStack.top();
-    	Tile* candidate = allTiles.front();
-    	allTiles.pop();
+    dumpTiles();
+    return 0;
+}
+
+void findNextMatch()
+{
+	printf("findNextMatch()\n");
+
+	// compare most recent solved tile with next available candidate tile
+	Tile* tile = solvedStack.top();
+
+	for (int i = 0; i < allTiles.size(); ++i) {
+
+		Tile* candidate = allTiles.front();
+		allTiles.pop();
 
     	if (matchTiles(tile, candidate, 1, 3)) {
     		solvedStack.push(candidate);
-    		printf("Match found\n");
+    		findNextMatch();
     	}
     	else {
     		allTiles.push(candidate);
-    		printf("Match not found\n");
     	}
-
-    	iterations++;
-    }
-
-    return 0;
+	}
 }
 
 // Takes two tile objects, compares tokens of tile1 at pos1 with tile2 at pos2. Rotates
 // tile2 all the way around until match is found
-bool matchTiles(Tile* tile1, Tile* tile2, int pos1, int pos2) {
+bool matchTiles(Tile* tile1, Tile* tile2, int pos1, int pos2)
+{
 
 	printf("matchTiles(tile1: %d, tile2: %d, pos1: %d, pos2: %d)\n", tile1->getId(), tile2->getId(), pos1, pos2);
 
     int tries = 4;
     while (tries > 0){
-        if (tile1->tokenAt(pos1) == tile2->tokenAt(pos2)) {
+        if (matchTokens(tile1->tokenAt(pos1), tile2->tokenAt(pos2))) {
               return true;
         }
         else {
@@ -77,10 +93,28 @@ bool matchTiles(Tile* tile1, Tile* tile2, int pos1, int pos2) {
     return false;
 }
 
+bool matchTokens(Token tok1, Token tok2)
+{
+	if (tok1 == TOK_BAR_A && tok2 == TOK_BAR_B) 		return true;
+	else if(tok1 == TOK_BAR_B && tok2 == TOK_BAR_A) 	return true;
+	else if(tok1 == TOK_GG_A && tok2 == TOK_GG_B) 		return true;
+	else if(tok1 == TOK_GG_B && tok2 == TOK_GG_A) 		return true;
+	else if(tok1 == TOK_PG_A && tok2 == TOK_PG_B) 		return true;
+	else if(tok1 == TOK_PG_B && tok2 == TOK_PG_A) 		return true;
+	else if(tok1 == TOK_TREE_A && tok2 == TOK_TREE_B) 	return true;
+	else if(tok1 == TOK_TREE_B && tok2 == TOK_TREE_A) 	return true;
+	else return false;
+
+}
+
 // Dumps an ascii representation of the current state of the solved puzzle
 void dumpTiles()
 {
-    // TODO
+	printf("SOLUTION:\n");
+	while(!solvedStack.empty()){
+    	printf("Tile number: %d\n", solvedStack.top()->getId());
+    	solvedStack.pop();
+    }
 }
 
 // Returns a string representation the token enum that is passed in
